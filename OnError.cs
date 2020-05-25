ec6685c1f2ba40845708b9bf6bb781a4a7473960
Daniel Strommen <nerdile@outlook.com>
2020-05-25 13:05:45 -0700

    Initial commit

---
+++ OnError.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NerdileSnippets.WinFormsHelpers
+{
+    public class OnError
+    {
+        public static async Task ShowMessageBox(Func<Task> fn)
+        {
+            try
+            {
+                await fn();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Something happened");
+            }
+        }
+
+        public static async Task LogToConsole(Func<Task> fn)
+        {
+            try
+            {
+                await fn();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
